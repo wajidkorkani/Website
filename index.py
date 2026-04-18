7c70a6b79da335d673c0213e90c6cfc4f0c5f38d
wajidkorkani <errormrcoder@gmail.com>
2026-04-18 23:10:24 +0500

    Add contact section

--- index.py
+++ index.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, redirect, request
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def About():
     return render_template("about.html")
 
+@app.route('/contact')
+def Contact():
+    return render_template("contact.html")
 
 app.run(debug=True)
